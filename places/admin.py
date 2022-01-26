a759978f497354633153bdf4f6051613c6681150
Boyko Alexey <elorian@yandex.ru>
2022-01-26 14:34:44 +0300

    Add to Django Administration Places images preview

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from .models import Place, Image
+from django.utils.safestring import mark_safe
 
 
 class ImageEdit(admin.TabularInline):
     model = Image
+    readonly_fields = ['get_preview']
+    def get_preview(self, obj):
+        return mark_safe(
+            '<img src="{url}" height=200 />'.format(
+                url=obj.image.url,
+            )
+        )
 
 
 @admin.register(Image)
@@ -12,8 +20,10 @@ class ImageAdmin(admin.ModelAdmin):
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
+    #readonly_fields = ['get_preview']
     list_display = ('id', 'title',)
     inlines = [ImageEdit,]
+
 
 
 #admin.site.register(Place, PlaceAdmin)
