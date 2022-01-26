9306c7da33f907a5d6b5bea7bfcb72e83fbdc196
Boyko Alexey <elorian@yandex.ru>
2022-01-26 13:58:59 +0300

    Add to Django Administration Places adititng images

--- places/admin.py
+++ places/admin.py
@@ -2,14 +2,20 @@ from django.contrib import admin
 from .models import Place, Image
 
 
+class ImageEdit(admin.TabularInline):
+    model = Image
+
+
+@admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'place')
 
-
+@admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('id', 'title',)
+    inlines = [ImageEdit,]
 
 
-admin.site.register(Place, PlaceAdmin)
-admin.site.register(Image, ImageAdmin)
+#admin.site.register(Place, PlaceAdmin)
+#admin.site.register(Image, ImageAdmin)
 
