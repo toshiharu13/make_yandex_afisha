6de115321a6690e4356387753164ef2cc7175e92
elf <elorian@yandex.ru>
2022-01-30 23:54:16 +0300

    correct variables, in <load_place> command

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -36,23 +36,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         response = requests.get(options["way_to"])
         response.raise_for_status()
-        response_json = response.json()
+        place_raw = response.json()
         try:
-            object_from_json = Place.objects.get_or_create(
-                title=response_json['title'],
-                description_short=response_json['description_short'],
-                description_long=response_json['description_long'],
-                lng=float(response_json['coordinates']['lng']),
-                lat=float(response_json['coordinates']['lat']),
+            object_from_link = Place.objects.get_or_create(
+                title=place_raw['title'],
+                description_short=place_raw['description_short'],
+                description_long=place_raw['description_long'],
+                lng=float(place_raw['coordinates']['lng']),
+                lat=float(place_raw['coordinates']['lat']),
             )[0]
-            for uri in response_json['imgs']:
+            for uri in place_raw['imgs']:
                 file_name = split_file_name(uri)
                 response = requests.get(uri)
                 response.raise_for_status()
-                image_from_json = Image()
-                image_from_json.image.save(file_name, ContentFile(response.content), save=False)
-                image_from_json.place = object_from_json
-                image_from_json.save()
-            print(object_from_json, object_from_json.id)
+
+                image_from_link = Image()
+                image_from_link.image.save(
+                    file_name, ContentFile(response.content), save=False)
+                image_from_link.place = object_from_link
+                image_from_link.save()
+            print(object_from_link, object_from_link.id)
         except BaseException as error:
             print(f'Cant get json file from that way- {error}')
