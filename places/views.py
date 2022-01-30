fb89636a22ed7bd2dff0ca3cdb6cf5d4fc230092
elf <elorian@yandex.ru>
2022-01-30 23:40:39 +0300

    correct variables, clear code in <places> view

--- places/views.py
+++ places/views.py
@@ -1,17 +1,18 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
 
 from places.models import Place
 
 
 def index(request):
-    place_data = {
+    geojson_for_template = {
         "type": "FeatureCollection",
         "features": []}
     all_places = Place.objects.all()
     for place in all_places:
-        place_data['features'].append(
+        geojson_for_template['features'].append(
             {
                 "type": "Feature",
                 "geometry": {
@@ -27,19 +28,18 @@ def index(request):
         )
 
     context = {
-        'all_places': place_data,
+        'all_places': geojson_for_template,
     }
     return render(request, 'index.html', context)
 
 
 def places(request, place_id):
-    need_place = Place.objects.filter(pk=place_id)
-    need_place_images = [
-        str(image) for image in need_place.first().images.all()]
-    need_place_json = list(need_place.values())
-    need_place_json[0]['imgs'] = need_place_images
+    needed_place = get_object_or_404(Place, pk=place_id)
+    needed_place_images = [str(image) for image in needed_place.images.all()]
+    needed_place_json = list(needed_place.values())
+    needed_place_json[0]['imgs'] = needed_place_images
     return JsonResponse(
-        need_place_json[0],
+        needed_place_json[0],
         safe=False,
         json_dumps_params={'indent': 2, 'ensure_ascii': False})
 
